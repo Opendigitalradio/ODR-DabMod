48b06743eded628962a9df94a7e1c662bf6fbee2
Matthias P. Braendli <matthias.braendli@mpb.li>
2015-06-07 09:24:03 +0200

    Always log to stderr
    Commit e3cc55d removed print of log to stderr by default, which is
    not helpful in most cases. This reverts this change.

--- src/Log.cpp
+++ src/Log.cpp
@@ -73,6 +73,8 @@ void Logger::logstr(log_level_t level, std::string message)
             ++it) {
         (*it)->log(level, message);
     }
+
+    std::cerr << levels_as_str[level] << " " << message << std::endl;
 }
 
 
