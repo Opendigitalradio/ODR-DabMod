b75afc42c95b76a0b789874f853ca8b49a290dd4
andreas128 <Andreas>
2017-08-18 16:34:58 +0200

    Add logging for timestamp

--- dpd/main.py
+++ dpd/main.py
@@ -53,7 +53,8 @@ coefs = adapt.get_coefs()
 model = Model.Model(coefs)
 
 for i in range(10):
-    txframe_aligned, _, rxframe_aligned, _ = meas.get_samples()
+    txframe_aligned, tx_ts, rxframe_aligned, rx_ts = meas.get_samples()
+    logging.debug("tx_ts {}, rx_ts {}".format(tx_ts, rx_ts))
     coefs = model.get_next_coefs(txframe_aligned, rxframe_aligned)
     adapt.set_coefs(coefs)
 
