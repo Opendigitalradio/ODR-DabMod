Opendigitalradio/ODR-DabMod

commit 1c110d11ae3d3181ed37c941fa7e622d332eb520

Silence Wmem-access warning in OfdmGenerator

src/OfdmGenerator.cpp
@@ -221,7 +221,8 @@ int OfdmGenerator::process(Buffer* const dataIn, Buffer* dataOut)
 
         if (myCfr) {
             reference.resize(mySpacing);
-            memcpy(reference.data(), myFftIn, mySpacing * sizeof(FFT_TYPE));
+            memcpy(reinterpret_cast<fftwf_complex*>(reference.data()),
+                    myFftIn, mySpacing * sizeof(FFT_TYPE));
         }
 
         fftwf_execute(myFftPlan); // IFFT from myFftIn to myFftOut
@@ -232,7 +233,8 @@ int OfdmGenerator::process(Buffer* const dataIn, Buffer* dataOut)
         if (myCfr) {
             if (myMERCalcIndex == i) {
                 before_cfr.resize(mySpacing);
-                memcpy(before_cfr.data(), myFftOut, mySpacing * sizeof(FFT_TYPE));
+                memcpy(reinterpret_cast<fftwf_complex*>(reference.data()),
+                        myFftOut, mySpacing * sizeof(FFT_TYPE));
             }
 
             /* cfr_one_iteration runs the myFftPlan again at the end, and

commit d2a45bf2b4a35fa0396936d0f5a6f2bb9d385e59

Fix CFR bug introduced in 1c110d1

src/OfdmGenerator.cpp
@@ -232,7 +232,7 @@ int OfdmGenerator::process(Buffer* const dataIn, Buffer* dataOut)
         if (myCfr) {
             if (myMERCalcIndex == i) {
                 before_cfr.resize(mySpacing);
-                memcpy(reinterpret_cast<fftwf_complex*>(reference.data()),
+                memcpy(reinterpret_cast<fftwf_complex*>(before_cfr.data()),
                         myFftOut, mySpacing * sizeof(FFT_TYPE));
             }
 