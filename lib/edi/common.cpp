69525b18ee7dc8cb6b56b047ed48bbe870f00e5f
Matthias P. Braendli <matthias.braendli@mpb.li>
2020-03-24 10:40:34 +0100

    Pull common code c8defb3

--- lib/edi/common.cpp
+++ lib/edi/common.cpp
@@ -310,12 +310,19 @@ bool TagDispatcher::decode_tagpacket(const vector<uint8_t> &payload)
         uint32_t taglength = read_32b(payload.begin() + i + 4);
 
         if (taglength % 8 != 0) {
-            etiLog.log(warn, "Invalid tag length!");
+            etiLog.log(warn, "Invalid EDI tag length, not multiple of 8!");
             break;
         }
         taglength /= 8;
 
         length = taglength;
+
+        const size_t calculated_length = i + 8 + taglength;
+        if (calculated_length > payload.size()) {
+            etiLog.log(warn, "Invalid EDI tag length: tag larger %zu than tagpacket %zu!",
+                    calculated_length, payload.size());
+            break;
+        }
 
         vector<uint8_t> tag_value(taglength);
         copy(   payload.begin() + i+8,
