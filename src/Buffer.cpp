7a473c10725b467b60cdf8011c22560b2250324b
Matthias P. Braendli <matthias.braendli@mpb.li>
2020-06-08 13:52:31 +0200

    Add missing includes for GCC 10 compatibility

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -29,8 +29,9 @@
 #include "PcDebug.h"
 
 #include <string>
-#include <stdlib.h>
-#include <string.h>
+#include <stdexcept>
+#include <cstdlib>
+#include <cstring>
 
 Buffer::Buffer(size_t len, const void *data)
 {
